be2fd81559bb10cd7f81a8940779797aa9c7eba0
Minao Tarosay Yamamoto <tarosa.y@gmail.com>
2026-06-28 12:10:44 +0900

    更新した

--- Programs/M5StampS3A/OpenLog_Sample_19200/OpenLog_Sample_19200.ino
+++ Programs/M5StampS3A/OpenLog_Sample_19200/OpenLog_Sample_19200.ino
@@ -3,6 +3,7 @@
 void setup() {
   Serial.begin(115200);
   Serial1.begin(19200, SERIAL_8N1, 1, 3);  //G1->RXD, G3->TXD);
+  //Serial1.begin(9600, SERIAL_8N1, 1, 3);  //G1->RXD, G3->TXD);
 
   delay(1500);
   Serial.println(F("Stamp S3 OpenLog Sample1_19200"));
